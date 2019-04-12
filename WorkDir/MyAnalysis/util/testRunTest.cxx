bcab02c3ae0faabcdfe421e0194ee817992046a1
Matthew Sullivan <msullivan@hep.ph.liv.ac.uk>
2019-04-12 14:01:34 +0100

    Rewritten submission scripts

---
+++ WorkDir/MyAnalysis/util/testRunTest.cxx
@@ -0,0 +1,241 @@
+#include "xAODRootAccess/Init.h"
+#include "SampleHandler/SampleHandler.h"
+#include "SampleHandler/fetch.h"
+#include "SampleHandler/ToolsDiscovery.h"
+#include "EventLoop/Job.h"
+#include <EventLoopGrid/PrunDriver.h>
+#include "EventLoop/DirectDriver.h"
+#include "SampleHandler/DiskListLocal.h"
+#include <TSystem.h>
+#include <EventLoopAlgs/NTupleSvc.h>
+#include <EventLoop/OutputStream.h>
+#include "testRun.h"
+
+#include <SampleHandler/MetaObject.h>
+#include <SampleHandler/Sample.h>
+#include <SampleHandler/SampleGrid.h>
+#include <SampleHandler/SampleHandler.h>
+
+#include <iostream>
+
+#include "MyAnalysis/MyxAODAnalysis.h"
+
+std::string get_sample_name(const std::string& s);
+std::string get_file_type(const std::string& s);
+bool is_multiple_submission(const std::string& s);
+void info_message(const std::string& s);
+
+int main( int argc, char* argv[]) {
+
+    std::string sample_path = "";
+    std::string sample_name = "";
+    std::string submit_dir = "";
+    std::string fileType = "";
+    std::string username = "";
+
+    bool RunningWithSyst = false;
+    bool RunningWithPhotons = false;
+    bool RunningLocally = true;
+    int NoEvents = -1;
+
+    if (argv[1] != "") sample_path = argv[1];
+    sample_name = get_sample_name(sample_path);
+    if (argv[2] != "") submit_dir  = argv[2] + sample_name + "/";
+    if (argv[3] != "") RunningWithSyst = (bool) atoi(argv[3]);
+    if (argv[4] != "") RunningWithPhotons = (bool) atoi(argv[4]);
+    if (argv[5] != "") RunningLocally = (bool) atoi(argv[5]);
+    if (argv[6] != "") NoEvents = atoi(argv[6]);
+    if (argv[7] != "") username = argv[7];
+    fileType = get_file_type(sample_name);
+
+    info_message("Input path: " + sample_path);
+    info_message("Sample name: " + sample_name);
+    info_message("Sample type: " + fileType);
+    info_message("Submission directory: " + submit_dir);
+    if (RunningWithSyst) info_message("Systematics: True" );
+    if (RunningWithPhotons) info_message("Photons: True" );
+    if (!RunningLocally) info_message("--- Going to submit to grid ---" );
+    if (NoEvents != -1) info_message("Events: " + std::to_string(NoEvents));
+
+    // Set up the job for xAOD access:
+    xAOD::Init().ignore();
+    // Construct the samples to run on:
+    SH::SampleHandler sh;
+    //job.options()->setString(EL::Job::optXaodAccessMode,EL::Job::optXaodAccessMode_branch);
+    if(RunningLocally){
+
+        SH::DiskListLocal list (sample_path);
+        SH::scanFiles(sh, list); // specifying one
+        sh.setMetaString ("nc_tree", "CollectionTree");
+        sh.print();
+        // Create an EventLoop job:
+        EL::Job job;
+        job.sampleHandler( sh );
+
+        job.options()->setString(EL::Job::optXaodAccessMode,EL::Job::optXaodAccessMode_class);
+        if (NoEvents != -1) job.options()->setDouble(EL::Job::optMaxEvents, NoEvents);
+        // Add our analysis to the job:
+        MyxAODAnalysis* alg = new MyxAODAnalysis();
+        EL::OutputStream output  ("output");
+        job.outputAdd (output);
+        job.algsAdd( alg );
+        //alg->setProperty("SampleName",inputFile);
+        alg->outputName = "outputLabel"; // give the name of the output to our algorithm
+        alg->inputFile = sample_name;
+        alg->doSyst = RunningWithSyst;
+        alg->doPhotons = RunningWithPhotons;
+        // If you want to check that the filtering is working correctly, then set this to false
+        alg->RunningLocally = RunningLocally;
+        alg->setMsgLevel(MSG::VERBOSE);
+
+        EL::DirectDriver driver;
+        job.options()->setDouble (EL::Job::optRemoveSubmitDir, 1);
+        
+        driver.submit( job, submit_dir );
+    }
+    else if (!RunningLocally){
+
+        std::string output_name = "";
+
+        std::vector<std::string> Months = {"Jan","Feb","Mar","Apr","May","June","July","Aug","Sept","Oct","Nov","Dec"};
+        std::time_t t = time(0);
+        struct tm * now = localtime( & t );
+        
+        int Month = now->tm_mon;
+        std::string CurrentDate = std::to_string(now->tm_mday)+Months[Month];
+        
+        std::string CreateDir = "/scratch/msullivan/GridSubmissions/"+CurrentDate+fileType;;
+        std::string Command = "[ ! -d "+CreateDir+" ] && mkdir "+CreateDir+" || echo \"Directory Exists\"";
+        std::system(Command.c_str());
+        
+        SH::scanRucio (sh, sample_name);
+        sh.print();
+        sh.setMetaString ("nc_tree", "CollectionTree");
+        sh.print();
+        
+        // Create an EventLoop job:
+        EL::Job job;
+        EL::PrunDriver driver;
+        job.sampleHandler( sh );
+        //job.options()->setString(EL::Job::optXaodAccessMode,EL::Job::optXaodAccessMode_branch);
+        job.options()->setString(EL::Job::optXaodAccessMode,EL::Job::optXaodAccessMode_class);
+        if (is_multiple_submission(sample_name)) {
+            output_name = CurrentDate + "." + "Combined." + fileType;
+            job.options()->setString( EL::Job::optSubmitFlags, "--addNthFieldOfInDSToLFN=2,3,6 --useContElementBoundary" );        
+            driver.options()->setString("nc_outputSampleName", "user." + username + "." + CurrentDate + "." + "Combined." + fileType+"_testsubmission");
+        } else {
+            output_name = CreateDir+"/"+sample_name;        
+            driver.options()->setString("nc_outputSampleName", "user." + username + "." + CurrentDate+"_"+fileType+"%in:name[2]%.%in:name[3]%"+"_testsubmission");
+        }
+        // Add our analysis to the job:
+        MyxAODAnalysis* alg = new MyxAODAnalysis();
+        
+        EL::OutputStream output  ("output");
+        job.outputAdd (output);
+        
+        job.algsAdd( alg );
+        alg->outputName = "outputLabel"; // give the name of the output to our algorithm
+        alg->inputFile = sample_name;
+        alg->doSyst = RunningWithSyst;
+        alg->doPhotons = RunningWithPhotons;
+        alg->RunningLocally = false;
+        alg->setMsgLevel(MSG::ERROR);
+
+        job.options()->setDouble (EL::Job::optRemoveSubmitDir, 1);
+
+        driver.options()->setString(EL::Job::optGridNFilesPerJob, "5");
+        // Use submit if you want to see all of the info about the submitted jobs. Use submitOnly if you want to send the jobs then Monitor online with panda
+        std::string out_dir = CreateDir + "/" + output_name;
+        driver.submitOnly( job, out_dir );
+
+    }
+  return 0;
+
+}
+
+std::string get_sample_name(const std::string& s)
+{
+   std::vector<std::string> tokens;
+   std::string token;
+   std::istringstream tokenStream(s);
+   while (std::getline(tokenStream, token, '/'))
+   {
+      tokens.push_back(token);
+   }
+   if (tokens.size() > 0) return tokens[tokens.size() - 1];
+   else return (s);
+}
+
+std::string get_file_type(const std::string& s)
+{
+
+    std::string fileType = "UNKNOWN";
+
+    int found_TRUTH1 = s.find("TRUTH1");
+    if (found_TRUTH1 != std::string::npos){
+      fileType = "TRUTH1";
+    }
+    int found_TRUTH3 = s.find("TRUTH3");
+    if (found_TRUTH3 != std::string::npos){
+      fileType = "TRUTH3";
+    }
+    int found_SUSY5 = s.find("SUSY5");
+    if (found_SUSY5 != std::string::npos){
+      fileType = "SUSY5";
+    }
+    int found_SUSY7 = s.find("SUSY7");
+    if (found_SUSY7 != std::string::npos){
+      fileType = "SUSY7";
+    }
+    int found_data15 = s.find("data15");
+    if (found_data15 != std::string::npos){
+      fileType = "DATA15";
+    }
+    
+    int found_data16 = s.find("data16");
+    if (found_data16 != std::string::npos){
+      fileType = "DATA16";
+    }
+    
+    int found_data17 = s.find("data17");
+    if (found_data17 != std::string::npos){
+      fileType = "DATA17";
+    }
+    
+    int found_data18 = s.find("data18");
+    if (found_data18 != std::string::npos){
+      fileType = "DATA18";
+    }
+    
+    int found_mc16a = s.find("r9364");
+    if(found_mc16a != std::string::npos){
+      fileType = "MC16a";
+    }
+    int found_mc16d = s.find("r10201");
+    if(found_mc16d != std::string::npos){
+      fileType = "MC16d";
+    }
+    int found_mc16e = s.find("r10724");
+    if(found_mc16e != std::string::npos){
+      fileType = "MC16e";
+    }
+
+    return fileType;
+}
+
+bool is_multiple_submission(const std::string& s) 
+{
+
+    int multi_sub = s.find(",");
+    if (multi_sub != std::string::npos){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+void info_message(const std::string& s) 
+{
+    std::cout << "\033[0;32m" << s << "\033[0;0m" << std::endl;
+}
