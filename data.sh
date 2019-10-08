b94cca3a23e9536e8e3adc20bb71e9f8dfed6f8e
Hamish Teagle <hamish.teagle@cern.ch>
2019-10-08 15:32:52 +0100

    removing jbutils, will be replaced by a fork

--- data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python listsubmission.py -i sample_lists/data15.txt
-python listsubmission.py -i sample_lists/data16.txt
-python listsubmission.py -i sample_lists/data17.txt
-python listsubmission.py -i sample_lists/data18.txt
