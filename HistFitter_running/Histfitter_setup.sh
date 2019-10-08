592a0372295214362e76e8585ae531860b58c935
Hamish Teagle <hamish.teagle@cern.ch>
2019-10-08 15:04:41 +0100

    Removing old histfitter setup, replaced in PyAnalysisUtils

--- HistFitter_running/Histfitter_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
-setupATLAS
-lsetup "root 6.06.06-x86_64-slc6-gcc49-opt"
-cd ../HistFitter
-source setup.sh
-cd src/
-make
-cd ../../HistFitter_running
--- HistFitter_running/makeContour_FitBoth.sh
+++
@@ -1,3 +0,0 @@
-GenerateJSONOutput.py -i results/OneLbb_MVA_Both_Output_hypotest.root -f "hypo_m%f_%f" -p "m0:m12"
-harvestToContours.py -i OneLbb_MVA_Both_Output_hypotest__1_harvest_list.json -x m0 -y m12 -l "x-125" --xMin 125 --yMin 0 --interpolation 'linear' -s '0.01'
-python  macros/Examples/contourPlotterExample/contourPlotterExample.py
