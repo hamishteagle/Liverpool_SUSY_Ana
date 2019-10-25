06301d2f87f3dca7342ff7bf915064048a42b310
Hamish Teagle <hamish.teagle@cern.ch>
2019-10-25 16:00:24 +0100

    source the build setup in the yaml

--- CI/release_setup.sh
+++ CI/release_setup.sh
@@ -5,9 +5,5 @@
 # Set up the base environment using the base image's setup script:
 source ~/analysis_release_setup.sh
 
-# Set up the GitLabCITests installation:
-source build/*-opt/setup.sh
-echo "Configured LivAnalysis from: build/*-opt/setup.sh it worked"
-
 # Set up the prompt:
 export PS1='\[\033[01;35m\][bash]\[\033[01;31m\][\u LivAnalysis-LivAnalysis]\[\033[01;34m\]:\W >\[\033[00m\] ';
