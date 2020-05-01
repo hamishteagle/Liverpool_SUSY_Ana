a645d5b62b57a81208b0c4f97836ca0fb571f077
Hamish Teagle <hamish.teagle@cern.ch>
2020-05-01 13:48:21 +0100

    Fixing full_test.py

--- WorkDir/Macros/Tests/full_test.py
+++ WorkDir/Macros/Tests/full_test.py
@@ -10,7 +10,11 @@ import unittest
 import argparse
 import sys
 
+import logging
+
 class Livana(unittest.TestCase):
+
+
     # maximum allowed difference between cutflows
     maxDiff = None
     t = None
@@ -24,6 +28,15 @@ class Livana(unittest.TestCase):
 
     # preserve location of this script
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+    def get_messaging(self):
+        self.msg=logging.getLogger("Cutflow Checker")
+        if Livana.args["debug"]: debug_level="DEBUG"
+        else:                    debug_level="INFO"
+        logging.basicConfig(level=debug_level,
+                            format='%(name)-12s :: %(levelname)-8s ::       %(message)s')
+
+        self.msg.info("Retrieved Messaging")
 
     ##Helper to pring the cutflows side-by-side if they don't match
     def cutflow_mismatch(self,current_flow, test_flow, e):
@@ -63,8 +76,12 @@ class Livana(unittest.TestCase):
     def write_cutflow(self, branch="CollectionTree_PFlow_"):
         cuts_dict = {}
         #Things are nicer if they're ordered in the json file
-        flow_dict = collections.OrderedDict()
-        flow_dict[branch]=collections.OrderedDict()
+
+        try:
+            self.flow_dict[branch]=collections.OrderedDict()
+        except:
+            self.flow_dict = collections.OrderedDict()
+            self.flow_dict[branch]=collections.OrderedDict()
         SumOfWgts = self.f.Get("h_SumOfWeights").Integral()
         with open(self.weights_json,'r') as ofile:
             weights_dict = json.load(ofile, object_pairs_hook=collections.OrderedDict)#Get the cuts from the json in the correct order
@@ -79,26 +96,32 @@ class Livana(unittest.TestCase):
             weighted_hist = ROOT.TH1F("weighted_hist","weighted_hist",1,0.5,1.5)
             self.t.Draw("1>>weighted_hist",cutnow+"*"+weighting)
             ##Write the cutflow as a tuple of unweighted and weighted events
-            flow_dict[branch][var] = self.t.GetEntries(str(cutnow)),weighted_hist.Integral()
+            self.flow_dict[branch][var] = self.t.GetEntries(str(cutnow)),weighted_hist.Integral()
             del weighted_hist
-            if (flow_dict[branch][var][0]==0):
+            if (self.flow_dict[branch][var][0]==0):
                 raise IOError('cut of "{0:s} {1:s}" is too tight (0 events) change this cut'.format(var, val))
             else:
-                print(self.blue(" {0:<20} | {1:^60}  | {2:^10}  | {3:>15}  ".format(str(branch), "cut: "+str(var)+" at: "+str(cuts_dict[var])," raw: "+str(flow_dict[branch][var][0])," weighted: "+str(flow_dict[branch][var][1]))))
+                if Livana.args["debug"]:
+                    print(self.blue(" {0:<20} | {1:^60}  | {2:^10}  | {3:>15}  ".format(str(branch), "cut: "+str(var)+" at: "+str(cuts_dict[var])," raw: "+str(self.flow_dict[branch][var][0])," weighted: "+str(self.flow_dict[branch][var][1]))))
         ##Write out when we've reached the last branch in the list
         if branch ==self.branch_list[-1]:
             with open(self.__location__+"/default/checkout_flow.json","w") as self.check_json:
-                json.dump(flow_dict,self.check_json)
+                self.msg.info("Dumping json to checkout_flow.json")
+                json.dump(self.flow_dict,self.check_json)
                 ##If we don't have a current cutflow to match, just copy the one we just made (will match by definition)
             if not os.path.exists(self.__location__+"/default/current_flow.json"):
+                self.msg.warn("No current_flow.json found->Overwriting with checkout_flow")
                 os.system("cp "+self.__location__+"/default/checkout_flow.json "+self.__location__+"/default/current_flow.json")
 
 
     def write_weights(self,branch="CollectionTree_PFlow_"):
-        if branch ==self.branch_list[-1]:
+
+        if branch ==self.branch_list[0]:
+            self.msg.info("Dumping json to checkout_weights")
             with open(self.__location__+"/default/checkout_weights.json","w") as self.checkout_weights_json:
                 json.dump(self.weights_output_dict,self.checkout_weights_json)
             if not os.path.exists(self.__location__+"/default/current_weights.json"):
+                self.msg.warn("No current_weights.json found->Overwriting with checkout_weights")
                 os.system("cp "+self.__location__+"/default/checkout_weights.json "+self.__location__+"/default/current_weights.json")
 
     def get_runfile(self, branch_number=0):
@@ -110,8 +133,9 @@ class Livana(unittest.TestCase):
             raise IOError('root file does not exist in given dir: "{0:s}"'.format(Livana.args["submit_dir"]))
         self.f = ROOT.TFile.Open(default_runfile_path[0])
         ##Get the list of branches
-        self.branch_list =[key.GetName() for key in self.f.GetListOfKeys() if isinstance(key.ReadObj(),ROOT.TTree)]
-        self.branch_list = list(set(self.branch_list))
+        self.branch_list = [key.GetName() for key in self.f.GetListOfKeys() if isinstance(key.ReadObj(),ROOT.TTree) and key.GetName()!= "CollectionTree_PFlow_"]
+        self.branch_list = list(set(self.branch_list))##Remove the doubled up branch names (ROOT is stupid)
+        self.branch_list.insert(0,"CollectionTree_PFlow_")
         self.t = self.f.Get(self.branch_list[branch_number])
         self.t.GetEntry(0)
 
@@ -138,10 +162,12 @@ class Livana(unittest.TestCase):
         return year_weight
 
     def test_cutflow(self):
+        self.get_messaging()
         self.get_runfile()
         self.get_cuts_json()
         self.get_weights_json()
         for i in range(len(self.branch_list)):
+            self.msg.info("Checking cutflow for branch: "+self.branch_list[i])
             #pick up the root files in the given submission dir
             default_runfile_path = glob.glob(os.path.join(Livana.args["submit_dir"],'*.root'))
             if len(default_runfile_path) >1:
@@ -153,72 +179,82 @@ class Livana(unittest.TestCase):
             #compare the flow we just wrote with the current one
         current_flow = collections.OrderedDict()
         test_flow    = collections.OrderedDict()
-        with open(self.__location__+"/default/current_flow.json",'r') as ofile:
-            current_flow = json.load(ofile, object_pairs_hook=collections.OrderedDict)#Get the cuts from the json in the correct order
+        ##Don't load the current flow if we're forcing a new one
+        if not Livana.args["force"]:
+            with open(self.__location__+"/default/current_flow.json",'r') as ofile:
+                current_flow = json.load(ofile, object_pairs_hook=collections.OrderedDict)#Get the cuts from the json in the correct order
         with open(self.__location__+"/default/checkout_flow.json",'r') as ofile:
             test_flow = json.load(ofile, object_pairs_hook=collections.OrderedDict)#Get the cuts from the json in the correct order
 
         if not Livana.args["force"]:
             try:
-                self.assertDictEqual(current_flow, test_flow, msg="cutflows don't match!!:")
+                for branch in self.branch_list:
+                    self.msg.debug("branch:"+branch)
+                    self.msg.debug("current dict:"+str(current_flow[branch]))
+                    self.msg.debug("test dict:"+str(test_flow[branch]))
+                    self.assertDictEqual(current_flow[branch], test_flow[branch], msg="cutflows don't match!!:")
             except AssertionError as e:
-                self.cutflow_mismatch(current_flow, test_flow, e)
+                self.cutflow_mismatch(current_flow[branch], test_flow[branch], e)
 
 
         #replace the current cutflow file with the new one. Need a metod to allow small changes here
-        os.remove(self.__location__+"/default/current_flow.json")
-        os.rename(self.__location__+"/default/checkout_flow.json", self.__location__+"/default/current_flow.json")
+        if Livana.args["force"]:
+            os.remove(self.__location__+"/default/current_flow.json")
+            os.rename(self.__location__+"/default/checkout_flow.json", self.__location__+"/default/current_flow.json")
 
     def test_weights(self):
-        if Livana.args["force"]:
-            return
-
+        self.get_messaging()
         self.weights_output_dict = collections.OrderedDict()
         self.get_weights_json()
         self.get_runfile()
 
         with open(self.weights_json,'r') as ofile:
             weights_dict = json.load(ofile, object_pairs_hook=collections.OrderedDict)#Get the cuts from the json in the correct order
-        with open(self.__location__+"/default/current_weights.json",'r') as ofile:
-            current_weights = json.load(ofile, object_pairs_hook=collections.OrderedDict)#Get the weights from the current weights dict
+        if not Livana.args["force"]:
+            with open(self.__location__+"/default/current_weights.json",'r') as ofile:
+                current_weights = json.load(ofile, object_pairs_hook=collections.OrderedDict)#Get the weights from the current weights dict
         for weight in weights_dict:
             self.weights_output_dict[weight]=collections.OrderedDict()
             nbins = int(weights_dict[weight].split(",")[0])
             xmin = float(weights_dict[weight].split(",")[1])
             xmax = float(weights_dict[weight].split(",")[2])
-            h = ROOT.TH1F("weight",str(weight),nbins,xmin,xmax)
-            h_current = ROOT.TH1F("weight_current",str(weight),nbins,xmin,xmax)
-            for bin_entry in current_weights[weight]:
-                bin  = h_current.SetBinContent(h_current.GetBin(int(bin_entry)),float(current_weights[weight][bin_entry]))
-            self.t.Draw(weight+">>weight")
-
-
             c = ROOT.TCanvas("Canvas1","Canvas1",0,0,900,900)
             Legend = ROOT.TLegend(0.60,0.76,0.80,0.94)
+            h = ROOT.TH1F("weight",str(weight),nbins,xmin,xmax)
+            h_current = ROOT.TH1F("weight_current",str(weight),nbins,xmin,xmax)
+            self.t.Draw(weight+">>weight")
+            if not Livana.args["force"]:
+                for bin_entry in current_weights[weight]:
+                    self.msg.debug("bin:"+str(bin_entry))
+                    self.msg.debug("entry:"+str(current_weights[weight][bin_entry]))
+                    bin  = h_current.SetBinContent(h_current.GetBin(int(bin_entry)),float(current_weights[weight][bin_entry]))
 
-            if not os.path.exists(os.path.join(Livana.__location__,"WeightPlots",self.branch_list[i])):
-                os.mkdir(os.path.join(Livana.__location__,"WeightPlots",self.branch_list[i]))
-            saveString =  os.path.join(Livana.__location__,"WeightPlots",self.branch_list[i],weight+'.pdf')
-            h_current.SetLineColor(ROOT.kGreen)
-            h_current.SetLineWidth(5)
-            h.SetLineColor(ROOT.kRed)
-            h_current.Draw("hist")
-            h.Draw("hist,same")
-            Legend.AddEntry(h_current,"Current","l")
-            Legend.AddEntry(h,"Commit","l")
-            Legend.Draw()
-            c.SaveAs(saveString)
+
+            if not Livana.args["force"]:
+                if not os.path.exists(os.path.join(Livana.__location__,"WeightPlots")):
+                    os.mkdir(os.path.join(Livana.__location__,"WeightPlots"))
+                saveString =  os.path.join(Livana.__location__,"WeightPlots",weight+'.pdf')
+                h_current.SetLineColor(ROOT.kGreen)
+                h_current.SetLineWidth(5)
+                h.SetLineColor(ROOT.kRed)
+                h_current.Draw("hist")
+                h.Draw("hist,same")
+                Legend.AddEntry(h_current,"Current","l")
+                Legend.AddEntry(h,"Commit","l")
+                Legend.Draw()
+                c.SaveAs(saveString)
             #Write the bins to the output dictionary
             for bin in range(nbins+1):
                 self.weights_output_dict[weight][str(bin)] = h.GetBinContent(bin)
             del h
             del h_current
             del c
-            self.write_weights(branch = self.branch_list[i])
+        self.write_weights()##Only write the weights on the nominal branch
         if not Livana.args["force"]:
             self.assertDictEqual(self.weights_output_dict,current_weights, msg="weights don't match!!")
-        os.remove(self.__location__+"/default/current_weights.json")
-        os.rename(self.__location__+"/default/checkout_weights.json", self.__location__+"/default/current_weights.json")
+        else:
+            os.remove(self.__location__+"/default/current_weights.json")
+            os.rename(self.__location__+"/default/checkout_weights.json", self.__location__+"/default/current_weights.json")
 
 
 
@@ -228,6 +264,7 @@ class Livana(unittest.TestCase):
 parser = argparse.ArgumentParser()
 parser.add_argument('-s','--submit-dir', required=False, metavar='submit_dir', type=str, help='Submit directory location', default='submitDir')
 parser.add_argument('-f','--force', action='store_true', required=False, dest='force', help='Force the submission (do not run the checks->Only run this if you want to update the nominal)', default=False)
+parser.add_argument('-d','--debug', action='store_true', required=False, dest='debug', help='Verbose debugging', default=False)
 
 Livana.args = vars(parser.parse_args())
 
